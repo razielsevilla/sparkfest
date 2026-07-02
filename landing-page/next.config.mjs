1edd5c38c3619bd432c70cf9f450e3ec683639ef
Jericho Varde <vardejericho@gmail.com>
2026-07-02 21:08:12 +0800

    feat: initialize Next.js configuration file

--- landing-page/next.config.mjs
+++ landing-page/next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    optimizePackageImports: ['lucide-react'],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
