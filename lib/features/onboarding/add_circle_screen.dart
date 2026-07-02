48cccf61598f97ec5bfb58ddfd8fd1518bf3ce5f
Jericho Varde <vardejericho@gmail.com>
2026-07-02 08:44:14 +0800

    feat: add AddCircleScreen to facilitate senior profile registration and initial trusted circle setup

--- lib/features/onboarding/add_circle_screen.dart
+++ lib/features/onboarding/add_circle_screen.dart
@@ -3,6 +3,7 @@ import 'package:gabaysr/core/theme/app_theme.dart';
 import 'package:gabaysr/core/services/app_state.dart';
 import 'package:gabaysr/models/senior_profile.dart';
 import 'package:gabaysr/models/trusted_circle_member.dart';
+import 'package:gabaysr/features/summary/family_dashboard.dart';
 
 class AddCircleScreen extends StatefulWidget {
   final AppState appState;
@@ -88,7 +89,13 @@ class _AddCircleScreenState extends State<AddCircleScreen> {
             backgroundColor: _primaryColor,
           ),
         );
-        Navigator.popUntil(context, (route) => route.isFirst);
+        Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(
+            builder: (context) => FamilyDashboard(appState: widget.appState),
+          ),
+          (route) => false,
+        );
       }
     } catch (e) {
       if (mounted) {
